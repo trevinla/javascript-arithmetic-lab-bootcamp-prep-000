a58c43e1c3b6d6d3e97441185e96f3942817a7b9
Trevor Seitz <trevinla@gmail.com>
2017-09-15 00:56:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b) {
+  var total = a + b;
+  return total;
+}
+
+function subtract(a,b) {
+  var total = a - b;
+  return total;
+}
+
+function multiply(a,b) {
+  var total = a * b;
+  return total;
+}
+
+function divide(a,b) {
+  var total = a / b;
+  return total;
+}
+
+function inc(n) {
+  n++;
+  return n;
+}
+
+function dec(n) {
+  n--;
+  return n;
+}
+
+function makeInt(n, b) {
+  parseInt(n, b);
+  return n;
+}
+
+function preserveDecimal(n) {
+  parseFloat(n);
+  return n;
+}
